57e21899d5dae7df3f3a030896b9c19f8ea72b25
Phearal <user.name>
2024-12-19 20:59:50 +0100

    refactor: calendar display

--- homecyclhome_admin/app/dashboard/plannings/Calendar.tsx
+++ homecyclhome_admin/app/dashboard/plannings/Calendar.tsx
@@ -4,20 +4,26 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import frLocale from '@fullcalendar/core/locales/fr';
 import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid'
 
-const events = [{ title: "Intervention", start: new Date() }];
+const events = [{ title: "Intervention", start: new Date("2024-12-19, 19:00:00") }];
 
 export default function Calendar() {
     return (
         <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
+            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+            initialView="timeGridWeek"
             weekends={false}
             events={events}
             eventContent={renderEventContent}
             locale={frLocale}
             selectable={true}
             dateClick={getInfo}
+            allDaySlot={false}
+            slotMinTime={"08:00:00"}
+            slotMaxTime={"20:00:00"}
+            height={"100%"}
+            eventClick={eventClick}
         />
     );
 }
@@ -37,6 +43,14 @@ function getInfo(info) {
         'Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY + '\n' +
         'Current view: ' + info.view.type
     );
-
-    alert('Current view: ' + info.view.type);
 }
+
+function eventClick(info) {
+    alert('Event: ' + info.event.title);
+    console.log(info);
+    console.log(info.event._instance.range.start.getHours());
+    
+
+    // change the border color just for fun
+    info.el.style.borderColor = 'red';
+  }
