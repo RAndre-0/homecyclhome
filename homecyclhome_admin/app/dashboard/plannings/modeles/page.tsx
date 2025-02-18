027ea22daab93b1eddb8d3140fc99acbf01052ca
ANDRE Raphaël <r.andre@agglo-pau.fr>
2025-02-18 17:08:14 +0100

    feat: model DELETE OK, Dialog bg todo

--- homecyclhome_admin/app/dashboard/plannings/modeles/page.tsx
+++ homecyclhome_admin/app/dashboard/plannings/modeles/page.tsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from "react";
 import { apiService } from "@/services/api-service";
 import { TypeIntervention } from "@/types/types";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { DeleteModelDialog } from "./DeleteModelDialog";
 
 interface InterventionModel {
     id: number;
@@ -20,6 +23,7 @@ export default function ModelesDePlanning() {
     const [models, setModels] = useState<Model[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const { toast } = useToast();
 
     useEffect(() => {
         const fetchModels = async () => {
@@ -28,7 +32,6 @@ export default function ModelesDePlanning() {
                 const detailedModels = await Promise.all(
                     data.map(async (model: { id: number; name: string }) => {
                         const detailedModel = await apiService(`modeles-planning/${model.id}`, "GET");
-                        console.log(detailedModel.modeleInterventions[0].interventionTime);
                         return detailedModel;
                     })
                 );
@@ -51,6 +54,15 @@ export default function ModelesDePlanning() {
         return date.toISOString().substring(11, 16);
     };
     
+    const deleteModel = async (id: number) => {
+        try {
+            await apiService(`modeles-planning/${id}`, "DELETE");
+            setModels(models.filter((model) => model.id !== id));
+            toast({ title: "Succès", description: "Modèle de planning supprimé avec succès." });
+        } catch (error) {
+            console.error("Erreur lors de la supression du modèle", error);
+        }
+    }
 
     if (loading) {
         return <div>Chargement en cours...</div>;
@@ -62,12 +74,16 @@ export default function ModelesDePlanning() {
 
     return (
         <div className="flex flex-col gap-5">
+            <div className="p-5 border rounded-lg">
+                <Button>Nouveau modèle</Button>
+            </div>
             <div className="flex flex-row gap-5">
-                <div className="p-5 w-1/3 border rounded-lg">
+                <div className="p-5 w-1/3 border rounded-lg flex flex-col gap-5">
                     <h1>Modèles</h1>
                     {models.map((model) => (
-                        <div key={model.id} onClick={() => setSelectedModel(model)} className="cursor-pointer">
-                            <h2>{model.name}</h2>
+                        <div className="flex items-center justify-between">
+                            <Button key={model.id} onClick={() => setSelectedModel(model)}>{model.name}</Button>
+                            <DeleteModelDialog modelId={model.id} onDelete={() => setModels(models.filter(m => m.id !== model.id))} />
                         </div>
                     ))}
                 </div>
@@ -76,8 +92,7 @@ export default function ModelesDePlanning() {
                         <div>
                             <h2>Détails du modèle: {selectedModel.name}</h2>
                             {selectedModel.modeleInterventions.map((intervention) => (
-                                <div key={intervention.id} className="mb-4">
-                                    {/* <p>Heure: {new Date(intervention.interventionTime).toLocaleTimeString()}</p> */}
+                                <div key={intervention.id} className={`mb-4 rounded-lg p-3 ${intervention.typeIntervention.nom === "Maintenance" ? "bg-emerald-400" : "bg-cyan-200"}`}>
                                     <p>Heure: {new Date(intervention.interventionTime).toLocaleTimeString("fr-FR", { hour: '2-digit', minute: '2-digit' })}</p>
                                     <p>Type: {intervention.typeIntervention.nom}</p>
                                     <p>Durée: {formatDuration(String(intervention.typeIntervention.duree))}</p>
